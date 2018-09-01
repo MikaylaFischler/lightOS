22cab7b854d52c3692dc582ff5093dee5b1ce47d
Mikayla Fischler <mafischler@wpi.edu>
2018-08-31 21:22:38 -0400

    port and other configs

---
+++ conf/config.h
@@ -0,0 +1,35 @@
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+// pin configuration (avoid pins 0 and 1)
+#define PIN_STRIP_1 6
+#define PIN_STRIP_2 7
+
+#define PIN_ESC_KEY 18
+#define PIN_BACK_KEY 19
+#define PIN_SEL_KEY 20
+#define PIN_NEXT_KEY 21
+
+#define PIN_LCD_RS 30
+#define PIN_LCD_EN 31
+#define PIN_LCD_D4 32
+#define PIN_LCD_D5 33
+#define PIN_LCD_D6 34
+#define PIN_LCD_D7 35
+
+#define RAND_SEED_ANALOG_NOISE_PORT A0
+
+// strip type
+#define STRIP_TYPE_RGB NEO_GRB + NEO_KHZ800
+
+// dimensions
+#define STRIP_LENGTH 19
+#define STRIP_1_LENGTH STRIP_LENGTH
+#define STRIP_2_LENGTH STRIP_LENGTH
+
+// strips
+#define ID_STRIP_NULL 0x0 // TODO put this somewhere else
+#define ID_STRIP_1 0x1
+#define ID_STRIP_2 0x2
+
+#endif
