55d7144d76ed253a9a2be5e08fa43c479cd58811
Mikayla Fischler <mafischler@wpi.edu>
2018-10-02 21:03:37 -0400

    DEBUG prints and start() of timer isr

--- os/boot/boot.cpp
+++ os/boot/boot.cpp
@@ -4,6 +4,15 @@ void boot(void) {
 	// allocate device storage
 	os::dev = (peripherals_t*) calloc(sizeof(peripherals_t), 1);
 
+	#ifdef DEBUG
+	Serial.begin(115200);
+	Serial.println("[~~~~~~~~~~     lightOS     ~~~~~~~~~~]");
+	Serial.print("System Version: ");
+	Serial.println(LIGHTOS_VERSION);
+	Serial.println("");
+	Serial.println("Boot initiated in DEBUG mode...");
+	#endif
+
 	// initialize LCD display
 	os::dev->out = new LCD(PIN_LCD_RS, PIN_LCD_EN, PIN_LCD_D4, PIN_LCD_D5, PIN_LCD_D6, PIN_LCD_D7);
 	os::display::init();
@@ -15,6 +24,9 @@ void boot(void) {
 	os::dev->out->print(F("lcd initialized "));
 	delay(500);
 	#endif
+	#ifdef DEBUG
+	Serial.println("LCD Initialized...");
+	#endif
 
 	// link devices to system
 	#ifndef FASTBOOT
@@ -22,12 +34,18 @@ void boot(void) {
 	os::dev->out->print(F("/dev init       "));
 	delay(200);
 	#endif
+	#ifdef DEBUG
+	Serial.println("Initializing devices...");
+	#endif
 
 	// setup additional devices
 	#ifndef FASTBOOT
 	os::dev->out->setCursor(0, 1);
 	os::dev->out->print(F("/dev init input "));
 	delay(200);
+	#endif
+	#ifdef DEBUG
+	Serial.println("Initializing inputs...");
 	#endif
 
 	// initialize keypad input
@@ -56,6 +74,9 @@ void boot(void) {
 	#else
 		if (status != 0) { boot_fail(); }
 	#endif
+	#ifdef DEBUG
+	Serial.println("\t> Init Escape Key...");
+	#endif
 
 	#ifndef FASTBOOT
 	os::dev->out->setCursor(0, 1);
@@ -80,6 +101,9 @@ void boot(void) {
 		}
 	#else
 		if (status != 0) { boot_fail(); }
+	#endif
+	#ifdef DEBUG
+	Serial.println("\t> Init Back Key...");
 	#endif
 
 	#ifndef FASTBOOT
@@ -106,6 +130,9 @@ void boot(void) {
 	#else
 		if (status != 0) { boot_fail(); }
 	#endif
+	#ifdef DEBUG
+	Serial.println("\t> Init Select Key...");
+	#endif
 
 	#ifndef FASTBOOT
 	os::dev->out->setCursor(0, 1);
@@ -131,12 +158,18 @@ void boot(void) {
 	#else
 		if (status != 0) { boot_fail(); }
 	#endif
+	#ifdef DEBUG
+	Serial.println("\t> Init Next Key...");
+	#endif
 
 	// initialize LEDs
 	#ifndef FASTBOOT
 	os::dev->out->setCursor(0, 1);
 	os::dev->out->print(F("/dev init led   "));
 	delay(200);
+	#endif
+	#ifdef DEBUG
+	Serial.println("Initializing LEDs...");
 	#endif
 	os::dev->leds = (ledstrips_t*) calloc(sizeof(ledstrips_t), 1);
 	os::dev->leds->num_strips = NUM_STRIPS;
@@ -147,6 +180,9 @@ void boot(void) {
 	os::dev->out->print(F(">init led 0     "));
 	delay(200);
 	#endif
+	#ifdef DEBUG
+	Serial.println("\t>Initializing Strip 0...");
+	#endif
 	os::dev->leds->strips[0] = new Adafruit_NeoPixel(STRIP_LENGTH, PIN_STRIP_1, STRIP_TYPE_RGB);
 	os::dev->leds->strips[0]->begin();
 	os::dev->leds->strips[0]->show();
@@ -155,6 +191,9 @@ void boot(void) {
 	os::dev->out->setCursor(0, 1);
 	os::dev->out->print(F(">init led 1     "));
 	delay(200);
+	#endif
+	#ifdef DEBUG
+	Serial.println("\t>Initializing Strip 1...");
 	#endif
 	os::dev->leds->strips[1] = new Adafruit_NeoPixel(STRIP_LENGTH, PIN_STRIP_2, STRIP_TYPE_RGB);
 	os::dev->leds->strips[1]->begin();
@@ -167,12 +206,18 @@ void boot(void) {
 	os::dev->out->print(F(">init os.ui     "));
 	delay(200);
 	#endif
+	#ifdef DEBUG
+	Serial.println("Initializing UI...");
+	#endif
 	os::ui::init();
 
 	#ifndef FASTBOOT
 	os::dev->out->setCursor(0, 1);
 	os::dev->out->print(F(">init os.ledctrl"));
 	delay(200);
+	#endif
+	#ifdef DEBUG
+	Serial.println("Initializing LED Control...");
 	#endif
 	os::led_ctrl::init();
 
@@ -181,9 +226,22 @@ void boot(void) {
 	os::dev->out->print(F(">boot complete  "));
 	delay(200);
 	#endif
+	#ifdef DEBUG
+	Serial.println("Boot Sequence Finished.");
+	Serial.println("");
+	#endif
+
+	// clear input status
+	os::dev->in.clearKeys();
+
+	// start timer interrupt
+	os::dev->in.start();
 }
 
 void boot_fail(void) {
+	#ifdef DEBUG
+	Serial.println("Fatal Boot Failure!");
+	#endif
 	os::dev->out->setCursor(0, 0);
 	os::dev->out->print(F(">>BOOT  FAILED<<"));
 	delay(250);
--- os/boot/boot.h
+++ os/boot/boot.h
@@ -1,6 +1,7 @@
 #ifndef LIGHT_OS_BOOT_H_
 #define LIGHT_OS_BOOT_H_
 
+#include <Arduino.h>
 #include "../../conf/config.h"
 
 void boot(void);
